0c8006023eb1c197a47a7b767e934796c4e55490
harazat <azat.harutyunyan3.y@tumo.org>
2019-07-19 18:57:58 +0400

    created api

--- routes/students.js
+++ routes/students.js
@@ -9,6 +9,29 @@ router.get('/students/:email', passport.authenticate('basic', { session: false }
   function(req, res, next) {
     // TODO: add api handler to check if email/password exists
     // TODO: Response back with user data
+    
+
+  if (req.params.email) {
+    db.getClient().collection("students").findOne({email: req.params.email}, 
+      function(err, results){
+        // if (results) {
+        //   res.send(results)
+        // }
+        // else{
+        //   res.status(400).send("Error")
+        // }
+          if(err){
+            res.status(500).send({error: err.message});
+          }
+          else if(!results){
+            res.status(400).send({error: 'Student does not exists'});
+          }
+          else{
+            res.send(results);
+          }
+      } );
+  }
+
 });
 
 /* Create a student account */
