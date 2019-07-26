63591ef1e6af9ea4b256f55f7110b5249e4c8491
Levon Eilazov <levon.eilazov@tumo.org>
2019-07-26 18:40:52 +0400

    final

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -45,53 +45,43 @@ class Profile extends Component {
       )
     }
     return (
-      <Container className="mt-5">
-        <h1 className="text-dark text-center">Profile</h1>
-        <Form className="mt-4" onSubmit={e => this.handleSubmit(e)}>
-          {
-            this.props.userError ?  <Alert variant="danger">{this.props.userError}</Alert>  : null
-          }
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control value={this.state.email} onChange={(e) => { this.handleChange("email", e.target.value) }} type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group controlId="formFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control value={this.state.firstName} onChange={(e) => { this.handleChange("firstName", e.target.value) }} type="text" placeholder="Enter first name" />
-          </Form.Group>
-          <Form.Group controlId="formLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control value={this.state.lastName} onChange={(e) => { this.handleChange("lastName", e.target.value) }} type="text" placeholder="Enter last name" />
-          </Form.Group>
-          <Form.Group controlId="formPassword">
-            <Form.Label>Passowrd</Form.Label>
-            <Form.Control value={this.state.password} onChange={(e) => { this.handleChange("password", e.target.value) }} type="password" placeholder="Enter password" />
-          </Form.Group>
-          <Form.Group controlId="formLearningTarget">
-            <Form.Label>Learning Targets</Form.Label>
-            <Form.Control value={this.state.learningTargets} onChange={(e) => { this.handleChange("learningTargets", e.target.options) }} as="select" multiple>
-              {
-                this.learningTargets.map(target => 
-                  <option value={target} key={target}>{target}</option>
-                )
-              }
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="formLocation">
-            <Form.Label>Location</Form.Label>
-            <Form.Control value={this.state.location} onChange={(e) => { this.handleChange("location", e.target.value) }} as="select">
-              {
-                this.locations.map(location => 
-                  <option key={location}>{location}</option>
-                )
-              }
-            </Form.Control>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Update
-          </Button>
-        </Form>
-      </Container>      
+      <Container className="mt-3">
+      <h1 class="display-4 text-secondary">Profile</h1>  <br></br> <br></br>
+      <Form onSubmit={this.handleSubmit}>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label> First Name</Form.Label>
+    <Form.Control onChange = {this.handleChange} type="email" disabled value= {this.props.user && this.props.user.firstName}/>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Last Name</Form.Label>
+    <Form.Control onChange = {this.handleChange} type="email" disabled value= {this.props.user && this.props.user.lastName}/>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Email</Form.Label>
+    <Form.Control onChange = {this.handleChange}type="email" disabled value= {this.props.user && this.props.user.email} />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Learning Targets</Form.Label>
+    <Form.Control onChange = {this.handleChange} type="email" disabled value= {this.props.user && this.props.user.learningTargets}/>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Location</Form.Label>
+    <Form.Control onChange = {this.handleChange} type="email" disabled value= {this.props.user && this.props.user.location}/>
+  </Form.Group>
+
+
+ 
+</Form>
+
+       
+
+        
+      </Container>  
+
     )
   }
 }
